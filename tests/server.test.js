810b28e590d20515e9b49efc0f4ee8ebfe2eaf6b
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-08-23 03:13:05 +0100

    added test to server

---
+++ tests/server.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest')
+const server = require('../api/server')
+const mongoose = require('mongoose')
+const { expectCt } = require('helmet')
+
+describe('index route', () => {
+  it('it runs', async (done) => {
+    const res = await request(server).get('/')
+    expect(res.body).toEqual({ message: 'API is up 🚀' })
+    expect(res.status).toBe(200)
+    done()
+  })
+  it('it returns URL cannot be found', async () => {
+    const res = await request(server).get('/server/auth')
+    expect(res.body).toEqual({ message: 'This URL can not be found' })
+    expect(res.status).toBe(404)
+  })
+})
+
+afterAll(async () => {
+  try {
+    await mongoose.connection.collections.users.drop()
+    await mongoose.connection.collections.fights.drop()
+    await mongoose.connection.collections.division.drop()
+    await mongoose.disconnect()
+  } catch (error) {
+    console.error(error.name, error.message)
+  }
+})
